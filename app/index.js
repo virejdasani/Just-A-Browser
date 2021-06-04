432aae5ec14f1c3df64e258c150116c4c7b74a90
Virej Dasani <dasanivirej@gmail.com>
2021-06-04 20:17:54 +0530

    Webview now dynamically resizes and websites can now be opened

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 var inputField = document.getElementById("inputField")
+var mainWebView = document.getElementById("mainWebView")
 let goButton = document.getElementById("goButton")
+
+// This is so that the webview looks normal every time the window is reloaded
+window.onload = resizeWebView()
 
 // When enter is pressed from the inputField
 goButton.addEventListener("click", (event) => {
@@ -12,8 +16,22 @@ goButton.addEventListener("click", (event) => {
     // This removes focus from the input field so the cursor doesn't keep blinking when not typing in the field
     inputField.blur()
 
-    console.log(website)
+    loadSiteToWebView(website)
 })
+
+// This is executed if the window is resized
+function resizeWebView() {
+    // The newWebViewWidth is the width of the window - 60px because the tabs container is 60px
+    let newWebViewWidth = window.innerWidth - 60
+    // This sets the new style to the mainWebViewContainer
+    document.getElementById("mainWebViewContainer").style.width = newWebViewWidth + "px"
+}
+
+// This filters and loads the website argument to the webview
+function loadSiteToWebView(website) {
+    document.querySelector("#mainWebView").setAttribute("src", website)
+
+}
 
 
 // For IPC

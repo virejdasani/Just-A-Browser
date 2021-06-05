d27378b6a03626ee070ea06e377fba1edc79c152
Virej Dasani <dasanivirej@gmail.com>
2021-06-05 23:41:08 +0530

    Add shortcut for ctrl + r

--- app/index.js
+++ app/index.js
@@ -6,16 +6,11 @@ const goButton = document.getElementById("goButton")
 // This is so that the webview looks normal every time the window is reloaded
 window.onload = resizeWebView()
 
-document.addEventListener("keyup", (e) => {
-    // if (e.key === "Meta" || e.key === "Control") {
-    //     // e.preventDefault()
-    //     console.log(e)
-    //     ctrlPressed = true
-    // }
-    // if (e.key === "r" && ctrlPressed === true) {
-    //     alert()
-    // }
-    // TODO - ctrl + r reloads
+// TODO - make this work with cmd and ctrl both
+document.addEventListener('keyup', (e) => {
+    if (e.ctrlKey && e.key === "r") {
+        alert("r")
+    }
 })
 
 // When enter is pressed from the inputField
@@ -31,8 +26,6 @@ goButton.addEventListener("click", (event) => {
     // This removes focus from the input field so the cursor doesn't keep blinking when not typing in the field
     inputField.blur()
 })
-
-
 
 
 
